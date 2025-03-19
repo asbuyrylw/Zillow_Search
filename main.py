312ed0aca3d860edd693df4502b871562b16650d
asbuyrylw <74421439+asbuyrylw@users.noreply.github.com>
2025-03-19 17:01:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from flask_cors import CORS
 from routes import routes
 from billing_routes import billing_routes
 from tasks import celery
+import os
+print("FLASK_ENV:", os.getenv("FLASK_ENV"))
+print("STRIPE_SECRET_KEY:", os.getenv("STRIPE_SECRET_KEY"))
+print("JWT_SECRET_KEY:", os.getenv("JWT_SECRET_KEY"))
+print("SCRAPEAK_API_KEY:", os.getenv("SCRAPEAK_API_KEY"))
+print("GOOGLE_CX_ID:", os.getenv("GOOGLE_CX_ID"))
+print("GOOGLE_API_KEY:", os.getenv("GOOGLE_API_KEY"))
+print("AWS_REGION:", os.getenv("AWS_REGION"))
+print("DYNAMODB_TABLE:", os.getenv("DYNAMODB_TABLE"))
+print("CELERY_BROKER_URL:", os.getenv("CELERY_BROKER_URL"))
+print("REDIS_URL:", os.getenv("REDIS_URL"))
+print("AWS_SECRET_MANAGER:", os.getenv("AWS_SECRET_MANAGER"))
 
 # ✅ Setup Logging
 logging.basicConfig(level=logging.INFO)
